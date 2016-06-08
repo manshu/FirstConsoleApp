1ed4958f74d0835ad6c3ec3ab216ed939dbedc11
Himanshu Batra <himanshu@batra.io>
2016-06-08 03:59:58 -0400

    Fixes

--- src/SpeakCommand.php
+++ src/SpeakCommand.php
@@ -1,6 +1,7 @@
 <?php 
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -11,12 +12,15 @@ class SpeakCommand extends Command
 	public function configure()
 	{
 		$this->setName('speak')
-				->setDescription('Speak a Message');
+				->setDescription('Speak a Message')
+				->addArgument('message', InputArgument::REQUIRED, 'What message should i speak ?');
 	}
 
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
-		exec('say "Say Your Name"');
+		exec('say ' . $input->getArgument('message'));
+
+		$output->writeln('All done'); 
 
 	}
 }
